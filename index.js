e0a44ac2d2381662cb45c2fc710bce5c41b8e0ce
fahmida-oni2 <onifahmida@gmail.com>
2025-11-25 23:01:11 +0600

    added the get operation

--- index.js
+++ index.js
@@ -21,13 +21,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("kit-db");
+    const kitCollection = db.collection("kit-collection");
+
+     app.get("/all-kits", async (req, res) => {
+      const result = await kitCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
